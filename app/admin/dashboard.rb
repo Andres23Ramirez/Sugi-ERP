82b95f4445fae9a39c6f7d6793b20038e0b0e1a4
Pedro Luis <dropeholguin1994@gmail.com>
2017-04-16 17:21:17 -0500

    Fixed dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,67 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+
+    columns do
+        column do
+            panel "Recent Establisment" do
+                ul do
+                    table_for Establishment.order("created_at desc").limit(5) do
+                        column :number
+                        column :name
+                        column :numberPhone
+                        column "CREATE DATE", :created_at
+                    end
+                    strong { link_to "View All Establishments", admin_establishments_path, class: 'button' }
+                end
+            end
+        end
+        column do
+            panel "Recent Entry" do
+                ul do
+                    table_for Entry.order("created_at desc").limit(5) do
+                        column :description
+                        column :amoun
+                        column :date
+                        column "CREATE DATE", :created_at
+                    end
+                    strong { link_to "View All Entry", admin_entries_path, class: 'button' }
+                end
+            end
+        end
     end
+
+    columns do
+        column do
+            panel "Recent Outflow" do
+                ul do
+                    table_for Outflow.order("created_at desc").limit(5) do
+                        column :description
+                        column :amoun
+                        column :date
+                        column "CREATE DATE", :created_at
+                    end
+                    strong { link_to "View All Outflow", admin_outflows_path, class: 'button' }
+                end
+            end
+        end
+        column do
+            panel "Recent Bill" do
+                ul do
+                    table_for Bill.order("created_at desc").limit(5) do
+                        column :number
+                        column "Establishment" do |bill|
+                          bill.establishment.name
+                        end
+                        column :amount
+                        column "CREATE DATE", :created_at
+                    end
+                    strong { link_to "View All Bill", admin_bills_path, class: 'button' }
+                end
+            end
+        end
+    end
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #
