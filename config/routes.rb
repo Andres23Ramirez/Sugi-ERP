388c1724fc11799c5892936a066892b056f34a32
Pedro Luis <pedro.holguin@railskarate.com>
2017-04-16 10:37:55 -0500

    Delete admin users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :contract_employees
   resources :employees
-  devise_for :admin_users
   resources :outflows
   resources :entries
   get 'financial/index'
