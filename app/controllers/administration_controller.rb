42d6396da33cc6b8e14f2b3cb464e1b846728594
Pedro Holguin <dropeholguin1994@gmail.com>
2017-05-05 18:53:07 -0500

    Added number and redirect in statistics

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -2,6 +2,9 @@ class AdministrationController < ApplicationController
 	before_action :authenticate_admin_user!
 	
 	def index
-		
+		@count_employees = Employee.all.count
+		@count_bills = Bill.all.count
+		@count_entries = Entry.all.count
+		@count_outflows = Outflow.all.count
 	end
 end
